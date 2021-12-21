0593f20bdf4367ae51f52e6e01ec81278659fa08
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-21 22:44:37 +0000

    context method

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,10 +1,10 @@
 describe '#even?' do
-  describe 'with even numbers' do
+  context 'with even numbers' do
     it 'should return true' do
       expect(4.even?).to eq true
     end
   end
-  describe 'with odd numbers' do
+  context 'with odd numbers' do
     it 'should return false' do
       expect(5.even?).to eq false
     end
