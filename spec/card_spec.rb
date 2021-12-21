742ecffc8de559369c2b11f31efa71bb44eabd88
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-21 22:24:58 +0000

    custom error message

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -12,10 +12,16 @@ describe Card do
  let(:card) {Card.new('Ace', 'Spades')}
   
  it 'has a rank' do
+  comparison = 'Ace'
   expect(card.rank).to eq 'Ace'
  end
 
  it 'has a suit' do
   expect(card.suit).to eq 'Spades'
+ end
+
+ it 'has custom error message' do
+  comparison = 'Spades'
+  expect(card.suit).to eq(comparison), "Hey I expected #{comparison}, but I got #{card.suit} instead..."
  end
 end
