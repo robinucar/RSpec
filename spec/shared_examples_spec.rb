fc734cc6cb81e7303d1bf842c98c45a18160ef04
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-24 00:03:23 +0000

    shared examples with include examples

---
+++ spec/shared_examples_spec.rb
@@ -0,0 +1,25 @@
+shared_examples 'a Ruby object with 3 elements' do
+  it 'returns the number of items' do
+    expect(subject.length).to eq 3
+  end
+end
+describe String do
+  subject { 'abc' }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+describe Hash do
+  subject{ { a: 1, b: 2, c: 3} }
+  include_examples 'a Ruby object with 3 elements'
+end
+
+class SausageLink
+  def length
+    3
+  end
+end
+
+describe SausageLink do
+  subject { described_class.new }
+  include_examples 'a Ruby object with 3 elements'
+end
