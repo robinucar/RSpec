9cb285717ac1bff20779d79ba89ef1e98e4c12ff
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-28 22:50:53 +0000

    receive counts

--- spec/mocks/movie_spec.rb
+++ spec/mocks/movie_spec.rb
@@ -29,9 +29,10 @@ class Movie
 
   def start_shooting
     if actor.ready?
-      puts actor.act
-      puts actor.fall_off_ladder
-      puts actor.light_on_fire
+      actor.act
+      actor.fall_off_ladder
+      actor.light_on_fire
+      actor.act
     end
   end
 end
@@ -44,13 +45,23 @@ movie.start_shooting
 describe Movie do
   let(:stunman) { double('Mr. Danger', ready?: true, act: 'Any String!', fall_off_ladder: 'Sure! Lets do it', light_on_fire: true) }
   subject {described_class.new(stunman)}
+
   describe '#start_shooting' do
+
     it 'expect an actor to do 3 actions' do
-      expect(stunman).to receive(:ready?)
-      expect(stunman).to receive(:act)
-      expect(stunman).to receive(:fall_off_ladder)
-      expect(stunman).to receive(:light_on_fire)
+      expect(stunman).to receive(:light_on_fire).once # once means that the method will be invoked one time
+      # alternative syntax
+      expect(stunman).to receive(:fall_off_ladder).exactly(1).times # the method will be invoked one time
+
+
+      expect(stunman).to receive(:light_on_fire).at_most(5).times # if the mothod invoked more than 5 times test will not pass
+
+      expect(stunman).to receive(:act).twice # alternative syntax: expect(stunman).to receive(:act).exactly(2).times
+
+      expect(stunman).to receive(:ready?).at_least(1).times # the method must be invoked at the least 1 time
+      
       subject.start_shooting # subject has to call start_shooting after receive all methods created in the stunman
+      
     end
   end
 end
