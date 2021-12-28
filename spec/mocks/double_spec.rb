0b0b19f611c1f41c51fed734c9fc4d9fd5a2f9f2
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-28 21:09:33 +0000

    created a test double

---
+++ spec/mocks/double_spec.rb
@@ -0,0 +1,18 @@
+describe 'a random double' do
+  it 'only allows defined method to be invoked' do
+    # stunman = double('Mr. Danger', fall_off_ladder: 'Ouch', light_on_fire: true) #accept multiple methods
+    # expect(stunman.fall_off_ladder).to eq('Ouch')
+    # expect(stunman.light_on_fire).to eq(true)
+
+    # alternative syntax
+    # stunman = double('Mr. Danger')
+    # allow(stunman).to receive(:fall_off_ladder).and_return('Ouch') # to receive method only accept ONE symbol as an argument.
+    # expect(stunman.fall_off_ladder).to eq('Ouch')
+
+    # alternative syntax (combination of both above syntaxes)
+    stunman = double('Mr. Danger')
+    allow(stunman).to receive_messages(fall_off_ladder: 'Ouch', light_on_fire: true) # accept multiple methods
+    expect(stunman.fall_off_ladder).to eq('Ouch')
+    expect(stunman.light_on_fire).to eq(true)
+  end
+end
